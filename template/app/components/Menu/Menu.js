678bf4ee6b77c599a3c5e69b3035fa38e932705f
Kamila <kamila.sroda@vp.pl>
2017-11-04 17:38:26 +0100

    react + webpack + babel + eslint

---
+++ template/app/components/Menu/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import style from './Menu.scss'
+
+const Menu = () => (
+  <ul className={style.menuUl}>
+    <li>One</li>
+    <li>Two</li>
+  </ul>
+)
+
+export default Menu
